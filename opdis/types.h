6b9dea0e38cc077488f4c783bd368f22668a914c
mkfs <semper.non.sequitur@gmail.com>
2010-01-26 16:49:14 -0500

    OPDIS : Base types documented

--- opdis/types.h
+++ opdis/types.h
@@ -1,6 +1,7 @@
 /*!
  * \file types.h
  * \brief Base types used by opdis
+ * \details This defines the types used by the opdis disassembler.
  * \author thoughtgang.org
  */
 
@@ -13,15 +14,53 @@
         #define LIBCALL
 #endif
 
+/*! \typedef bfd_byte opdis_byte_t
+ *  \ingroup types
+ *  \brief A byte.
+ */
 typedef bfd_byte opdis_byte_t;
+
+/*! \typedef size_t opdis_off_t;
+ *  \brief A buffer offset.
+ */
 typedef size_t opdis_off_t;
+
+/*! \def OPDIS_INVALID_OFFSET
+ *  An invalid buffer offset.
+ *  \ingroup types
+ *  \sa opdis_off_t
+ */
+#define OPDIS_INVALID_OFFSET ((opdis_off_t) -1 )
+
+/*! \typedef bfd_vma opdis_addr_t;
+ *  \ingroup types
+ *  A virtual memory (load) address.
+ */
 typedef bfd_vma opdis_addr_t;
 
+/*! \def OPDIS_INVALID_ADDR
+ *  An invalid address.
+ *  \ingroup types
+ *  \sa opdis_addr_t
+ */
+#define OPDIS_INVALID_ADDR ((opdis_addr_t) -1 )
+
+/*! \struct opdis_buffer_t
+ *  \ingroup types
+ *  \brief A buffer containing bytes to disassemble.
+ */
 typedef struct {
-	opdis_off_t 		len;
-	opdis_byte_t * 		data;
+	opdis_off_t 	len;	/*!< Number of bytes in buffer. */
+	opdis_byte_t * 	data;	/*!< Contents of buffer. */
 } opdis_buffer_t;
+
+/*! \typedef opdis_buffer_t * opdis_buf_t
+ *  \ingroup types
+ *  \brief Pointer to an opdis buffer.
+ */
 typedef opdis_buffer_t * opdis_buf_t;
+
+/* ---------------------------------------------------------------------- */
 
 #ifdef __cplusplus
 extern "C"
@@ -29,20 +68,55 @@ extern "C"
 #endif
 
 /*!
- * \fn
- * \brief
- * \param
- * \relates
- * \sa
+ * \fn opdis_buf_t opdis_buf_alloc( opdis_off_t )
+ * \ingroup types
+ * \brief Allocate an opdis buffer
+ * \details Allocates an opdis_buffer_t of specified size. The buffer contents
+ *          are initialized to zero.
+ * \param size
+ * \return The allocated opdis buffer.
+ * \sa opdis_buf_read opdis_buf_free
  */
 
 opdis_buf_t LIBCALL opdis_buf_alloc( opdis_off_t size );
 
-opdis_buf_t LIBCALL opdis_buf_read( FILE * f );
+/*!
+ * \fn opdis_buf_t opdis_buf_read( FILE *, opdis_off_t )
+ * \ingroup types
+ * \brief Allocate an opdis buffer containing contents of file
+ * \details Allocates an opdis buffer and fills it with the contents of
+ *          the file. This reads \e size bytes from the current position
+ *          in \e f, or from the current position to the end of the file
+ *          if \e size is 0. The current position in \e f is restored 
+ *          on return.
+ * \param f The file to read from.
+ * \param size The number of bytes to read from the file, or 0.
+ * \return The allocated opdis buffer.
+ * \sa opdis_buf_alloc opdis_buf_free
+ */
+opdis_buf_t LIBCALL opdis_buf_read( FILE * f, opdis_off_t size );
 
+/*!
+ * \fn int opdis_buf_fill( opdis_buf_t, opdis_off_t, void *, opdis_off_t )
+ * \ingroup types
+ * \brief Fill an opdis buffer from a memory location
+ * \details
+ * \param buf Opdis buffer to fill.
+ * \param offset Offset in buf to copy to.
+ * \param src Memory location to copy from.
+ * \param len Number of bytes to copy from src to buf.
+ * \return Number of bytes copied from src to buf.
+ */
 int LIBCALL opdis_buf_fill( opdis_buf_t buf, opdis_off_t offset,
 			    void * src, opdis_off_t len );
 
+/*!
+ * \fn void opdis_buf_free( opdis_buf_t )
+ * \ingroup types
+ * \brief Free an opdis buffer.
+ * \param buf Opdis buffer to free.
+ * \sa opdis_buf_alloc
+ */
 void LIBCALL opdis_buf_free( opdis_buf_t buf );
 
 #ifdef __cplusplus
