d111579a7b5fad53e25667c7b82885fb991d5d42
mkfs <semper.non.sequitur@gmail.com>
2010-02-16 19:40:26 -0500

    OPDIS : Fleshed out most of job list implementation

--- src/job_list.c
+++ src/job_list.c
@@ -1,45 +1,166 @@
 /* job_list.c
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "job_list.h"
 
 /* allocate a job list */
 job_list_t job_list_alloc( void ) {
-	return NULL;
+	return (job_list_t) calloc( 1, sizeof(struct JOB_LIST_HEAD) );
 }
 
 /* free an allocated job list */
 void job_list_free( job_list_t jobs ) {
+	job_list_item_t * item, * next;
+
+	if (! jobs ) {
+		return;
+	}
+
+	for ( item = jobs->head; item; item = next ) {
+		next = item->next;
+		free( item );
+	}
+
+	free( jobs );
 }
 
 /* add a memspec job to a job list */
-unsigned int job_list_add( job_list_t jobs, enum job_type_t type, 
-			   unsigned int target, opdis_off_t offset, 
-			   opdis_vma_t vma, opdis_off_t size ) {
-	return 0;
+static job_list_item_t * add_item( job_list_t jobs, enum job_type_t type, 
+			   const char * spec, unsigned int target ) {
+	job_list_item_t * item, *prev;
+
+	if (! jobs || ! spec ) {
+		return 0;
+	}
+
+	item = (job_list_item_t *) calloc( 1, sizeof(job_list_item_t) );
+	if (! item ) {
+		return 0;
+	}
+
+	item->type = type;
+	item->spec = spec;
+	item->target = target;
+
+	if (! jobs->head ) {
+		jobs->head = item;
+	} else {
+		for ( prev = jobs->head; prev->next; prev = prev->next )
+			;
+		prev->next = item;
+	}
+
+	jobs->num_items++;
+
+	return item;
 }
+
+unsigned int job_list_add( job_list_t jobs, enum job_type_t type, 
+			   const char * spec, unsigned int target, 
+			   opdis_off_t offset, opdis_vma_t vma, 
+			   opdis_off_t size ) {
+	job_list_item_t * item = add_item( jobs, type, spec, target );
+	if (! item ) {
+		return 0;
+	}
+
+	item->offset = offset;
+	item->vma = vma;
+	item->size = size;
+
+	return jobs->num_items;
+}
+
 
 /* add a bfd job to a job list */
 unsigned int job_list_add_bfd( job_list_t jobs, enum job_type_t type, 
-			       unsigned int target, const char * bfd_name ) {
-	return 0;
+			       const char * spec, unsigned int target, 
+			       const char * bfd_name ) {
+	job_list_item_t * item = add_item( jobs, type, spec, target );
+	if (! item ) {
+		return 0;
+	}
+
+	item->bfd_name = bfd_name;
+
+	return jobs->num_items;
 }
 
 /* invoke a callback for every job in list */
 void job_list_foreach( job_list_t jobs, JOB_LIST_FOREACH_FN fn, void * arg ) {
+	job_list_item_t * item;
+	unsigned int id = 1;
+
+	if (! jobs || ! fn ) {
+		return;
+	}
+
+	for ( item = jobs->head; item; item = item->next, id++ ) {
+		fn( item, id, arg );
+	}
+}
+
+static int perform_job( job_list_item_t * job, tgt_list_t targets,
+			mem_map_t map, opdis_t o ) {
+	if (! job || ! targets || ! map || ! o ) {
+		return 0;
+	}
+
+	// TODO: switch on job type
+	return 0;
 }
 
 /* perform the specified job */
-int job_list_perform( job_list_t jobs, unsigned int item, tgt_list_t targets,
+int job_list_perform( job_list_t jobs, unsigned int id, tgt_list_t targets,
 		      mem_map_t map, opdis_t o ) {
+	job_list_item_t * item;
+	unsigned int curr_id = 1;
+
+	if (! jobs ) {
+		return;
+	}
+
+	for ( item = jobs->head; item; item = item->next, curr_id++ ) {
+		if ( curr_id == id ) {
+			return perform_job( item, targets, map, o );
+		}
+	}
+
 	return 0;
 }
 
 /* perform all jobs */
 int job_list_perform_all( job_list_t jobs , tgt_list_t targets, mem_map_t map, 
-			  opdis_t o ) {
-	return 0;
+			  opdis_t o, int quiet ) {
+	job_list_item_t * item;
+	int rv = 1;
+
+	if (! jobs ) {
+		return;
+	}
+
+	for ( item = jobs->head; item; item = item->next ) {
+		int result = perform_job( item, targets, map, o );
+		if (! result ) {
+		}
+		rv &= result;
+	}
+
+	return rv;
+}
+
+static void print_job( job_list_item_t * job, unsigned int id, void * arg ) {
+	FILE * f = (FILE *) arg;
+	if (! f ) {
+		return;
+	}
+	
+	// TODO
 }
 
 void print_job_list( job_list_t jobs, FILE * f ) {
+	job_list_foreach( jobs, print_job, f );
 }
--- src/job_list.h
+++ src/job_list.h
@@ -21,7 +21,7 @@ enum job_type_t {
 typedef struct JOB_LIST_ITEM {
 	enum job_type_t type;	/* type of job */
 	const char * spec;	/* string value for job */
-	unsigned int tgt;	/* target of job */
+	unsigned int target;	/* target of job */
 	const char * bfd_name;	/* BFD argument (if applicable) */
 	opdis_off_t offset;	/* Offset argument for job */
 	opdis_vma_t vma;	/* VMA argument for job */
@@ -31,7 +31,7 @@ typedef struct JOB_LIST_ITEM {
 } job_list_item_t;
 
 typedef struct JOB_LIST_HEAD {
-	unsigned int	num_jobs;
+	unsigned int	num_items;
 	job_list_item_t * head;
 } * job_list_t;
 
@@ -44,25 +44,28 @@ job_list_t job_list_alloc( void );
 void job_list_free( job_list_t );
 
 /* add a memspec job to a job list */
-unsigned int job_list_add( job_list_t, enum job_type_t type, 
+unsigned int job_list_add( job_list_t, enum job_type_t type, const char * spec,
 			   unsigned int target, opdis_off_t offset, 
 			   opdis_vma_t vma, opdis_off_t size );
 
 /* add a bfd job to a job list */
 unsigned int job_list_add_bfd( job_list_t, enum job_type_t type, 
-			       unsigned int target, const char * bfd_name );
+			       const char * spec, unsigned int target, 
+			       const char * bfd_name );
 
-typedef void (*JOB_LIST_FOREACH_FN) ( job_list_item_t *, void * );
+typedef void (*JOB_LIST_FOREACH_FN) ( job_list_item_t *, unsigned int id, 
+				      void * );
 
 /* invoke a callback for every job in list */
 void job_list_foreach( job_list_t, JOB_LIST_FOREACH_FN, void * arg );
 
 /* perform the specified job */
-int job_list_perform( job_list_t, unsigned int item, tgt_list_t,
+int job_list_perform( job_list_t, unsigned int id, tgt_list_t,
 		      mem_map_t, opdis_t );
 
 /* perform all jobs */
-int job_list_perform_all( job_list_t, tgt_list_t, mem_map_t, opdis_t );
+int job_list_perform_all( job_list_t, tgt_list_t, mem_map_t, opdis_t,
+			  int quiet );
 
 void print_job_list( job_list_t, FILE * f );
 #endif
