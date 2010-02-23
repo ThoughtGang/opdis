8e0bb3577d3e26f1f3b09155fe2c03163c1dbf2a
mkfs <semper.non.sequitur@gmail.com>
2010-02-22 19:23:11 -0500

    OPDIS : Added empty check to insn_buf_clear

--- opdis/insn_buf.c
+++ opdis/insn_buf.c
@@ -76,8 +76,10 @@ int LIBCALL opdis_insn_buf_append( opdis_insn_buf_t buf, const char * item ) {
 }
 
 void LIBCALL opdis_insn_buf_clear( opdis_insn_buf_t buf ) {
-	buf->item_count = 0;
-	buf->string[0] = '\0';
+	if ( buf ) {
+		buf->item_count = 0;
+		buf->string[0] = '\0';
+	}
 }
 
 void LIBCALL opdis_insn_buf_free( opdis_insn_buf_t buf ) {
